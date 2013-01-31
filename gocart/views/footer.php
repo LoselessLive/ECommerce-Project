075cae4a1cd80ade22ae2c4fde5dbb83ca2027ab
Pongpong <LoselessLive@gmail.com>
2013-01-31 20:00:05 +0700

    Main Program

---
+++ gocart/views/footer.php
@@ -0,0 +1,11 @@
+	<footer class="footer">
+		<p class="pull-right">
+			<a href="http://clearskydesigns.com" target="_blank"><img src="https://s3.amazonaws.com/clearsky/csd_full_dark.png" alt="Clear Sky Designs" /></a>
+		</p>
+		<p>&copy; <?php echo date('Y')?> Clear Sky Designs &bull; All Rights Reserved</p>
+	</footer>
+</div>
+
+
+</body>
+</html>
